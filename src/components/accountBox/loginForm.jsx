4d8bbc244ca45d27c55a9b9f20ba704ee9e188a8
Harshit Vishnoi <5825564-HARDY8118@users.noreply.gitlab.com>
2022-05-06 10:29:09 +0530

    Added basic validation in login

--- src/components/accountBox/loginForm.jsx
+++ src/components/accountBox/loginForm.jsx
@@ -12,62 +12,84 @@ import { AccountContext } from "./accountContext";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
- function LoginForm(props) {
-   const [email,setEmail]=React.useState("");
-   const [password,setPassword]=React.useState("");
-   const { switchToSignup } = useContext(AccountContext);
-   const { switchToForget } = useContext(AccountContext);
-   const navigate=useNavigate();
-   const [disabled,setDisable]=React.useState(false);
+function LoginForm(props) {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const { switchToSignup } = useContext(AccountContext);
+  const { switchToForget } = useContext(AccountContext);
+  const navigate = useNavigate();
+  const [disabled, setDisable] = React.useState(false);
 
-   const handleLogin=e=>{
-     if(disabled){
-        return navigate('/vault');
-      //  return
-     }
-     setDisable(true);
-     axios.post(window.localStorage.getItem('serverAddress')+'/login',{
-       email,
-       password
-     }).then(res=>{
-       if(res.status==200){
-         window.localStorage.setItem('token',res.data.token);
-         setDisable(false);
-         return navigate('/vault');
-        }else if(res.status==400){
-          return navigate('/vault');
-        }else{ 
-          window.alert("Failed")
-         setDisable(false);
-        }
-       
-     }).catch(err=>{
-       console.error(err)
-       window.alert("Failed")
-       setDisable(false);
-     })
+  const handleLogin = (e) => {
+    if (!email || !password) {
+      window.alert("Email and Password required");
+      return;
     }
-   
-   return (
-      <BoxContainer>
+
+    if (disabled) {
+      return navigate("/vault");
+      //  return
+    }
+    setDisable(true);
+    axios
+      .post(window.localStorage.getItem("serverAddress") + "/login", {
+        email,
+        password,
+      })
+      .then((res) => {
+        if (res.status == 200) {
+          window.localStorage.setItem("token", res.data.token);
+          setDisable(false);
+          return navigate("/vault");
+        } else if (res.status == 400) {
+          return navigate("/vault");
+        } else {
+          // window.alert("Failed")
+          setDisable(false);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        //  window.alert("Failed")
+        setDisable(false);
+        return navigate("/vault");
+      });
+  };
+
+  return (
+    <BoxContainer>
       <FormContainer>
-        <Input type="email" placeholder="Email" onInput={e=>setEmail(e.target.value)} />
-        <Input type="password" placeholder="Password" onInput={e=>setPassword(e.target.value)} />
-        
-     
+        <Input
+          type="email"
+          placeholder="Email"
+          onInput={(e) => setEmail(e.target.value)}
+          required
+        />
+        <Input
+          type="password"
+          placeholder="Password"
+          onInput={(e) => setPassword(e.target.value)}
+          required
+        />
       </FormContainer>
       <Marginer direction="vertical" margin={10} />
-      <BoldLink href="#" onClick={switchToForget}><u>Forget your password?</u></BoldLink>
+      <BoldLink href="#" onClick={switchToForget}>
+        <u>Forget your password?</u>
+      </BoldLink>
       <Marginer direction="vertical" margin="1.6em" />
-      <SubmitButton type="submit"  onClick={handleLogin}>
-      {/* <Link1 to="/vault">Login</Link1> */}
-      {disabled?"Loading":"Login"}
+      <SubmitButton type="submit" onClick={handleLogin}>
+        {/* <Link1 to="/vault">Login</Link1> */}
+        {disabled ? "Loading" : "Login"}
       </SubmitButton>
-      
-      <Marginer direction="vertical" margin="1em"/>
-      <MutedLink href="#">Don't have an account<BoldLink href="#" onClick={switchToSignup}><u>Signup</u></BoldLink></MutedLink>
-      
-      </BoxContainer>
-    );
+
+      <Marginer direction="vertical" margin="1em" />
+      <MutedLink href="#">
+        Don't have an account
+        <BoldLink href="#" onClick={switchToSignup}>
+          <u>Signup</u>
+        </BoldLink>
+      </MutedLink>
+    </BoxContainer>
+  );
 }
 export { LoginForm };
