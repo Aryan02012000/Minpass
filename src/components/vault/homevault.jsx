c8b84e88596ccd5a408f9e65e18f96c78da27fd4
Harshit Vishnoi <5825564-HARDY8118@users.noreply.gitlab.com>
2022-05-08 13:49:06 +0530

    Fixed class error

--- src/components/vault/homevault.jsx
+++ src/components/vault/homevault.jsx
@@ -1,16 +1,15 @@
 //import { useSpring } from "framer-motion";
 import React, { useState, useEffect, Fragment } from "react";
-import { nanoid } from 'nanoid';
+import { nanoid } from "nanoid";
 import styled from "styled-components";
 import { Marginer } from "../marginer";
 import { Nav } from "./Nav";
 import axios from "axios";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import { Button } from "../homepage/button";
 //import { HiddenItem } from "./HiddenItem"
 import ReadOnlyRow from "./ReadOnlyRow";
 import EditableRow from "./EditableRow";
-
 
 const AppContainer = styled.div`
   width: 100%;
@@ -28,7 +27,7 @@ const AppContainer = styled.div`
 //   display: flex;
 //   flex-direction: column;
 //   align-items: center;
- 
+
 // `;
 
 const InnerContainer = styled.div`
@@ -47,23 +46,21 @@ const HomeVault = () => {
   const [users, setUser] = useState([]);
   const navigate = useNavigate();
   useEffect(() => {
-    if (!localStorage.getItem('token')) {
+    if (!localStorage.getItem("token")) {
       loadUsers();
     } else {
-      navigate('/signup')
+      navigate("/signup");
     }
   }, []);
 
-  
-
-  const [editUserId,setEditUserId] = useState(null);
+  const [editUserId, setEditUserId] = useState(null);
 
   const [info, setinfo] = useState(false);
 
   const loadUsers = async () => {
     const result = await axios.get("http://localhost:3003/users");
     setUser(result.data);
-  }
+  };
   const [addFormData, setAddFormData] = useState({
     name: " ",
     username: " ",
@@ -74,14 +71,12 @@ const HomeVault = () => {
     name: " ",
     username: " ",
     password: " ",
-  })
-
-
+  });
 
   const hanleAddFormChange = (event) => {
     event.preventDefault();
 
-    const fieldName = event.target.getAttribute('name');
+    const fieldName = event.target.getAttribute("name");
     const fieldValue = event.target.value;
 
     const newFormData = { ...addFormData };
@@ -96,11 +91,11 @@ const HomeVault = () => {
     const fieldName = event.target.getAttribute("name");
     const fieldValue = event.target.value;
 
-    const newFormData= { ...editFormData};
+    const newFormData = { ...editFormData };
     newFormData[fieldName] = fieldValue;
 
     setEditFormData(newFormData);
-  }
+  };
 
   const handleAddFormSubmit = (event) => {
     event.preventDefault();
@@ -117,97 +112,128 @@ const HomeVault = () => {
   };
 
   const handleEditFormSubmit = (event) => {
-     event.preventDefault();
+    event.preventDefault();
 
-     const editedContact = {
+    const editedContact = {
       id: editUserId,
       name: editFormData.name,
       username: editFormData.username,
       password: editFormData.password,
-     }
-
-     const newUsers = [...users];
-
-     const index= users.findIndex((user)=> user.id === editUserId);
-
-     newUsers[index] = editedContact;
-
-     setUser(newUsers);
-     setEditUserId(null);
-  };
-
-  const handleEditClick = (event, user)=> {
-      event.preventDefault();
-      setEditUserId(user.id);
-
-      const formValues={
-        name: user.name,
-        username: user.username,
-        password: user.password,
-      }
-
-      setEditFormData(formValues);
     };
 
-    const handleDeleteClick = (userId) => {
-      const newUsers = [...users];
-      
-      const index = users.findIndex((user)=> user.id === userId);
-      
-      newUsers.splice(index, 1);
+    const newUsers = [...users];
 
-      setUser(newUsers);
-    }
+    const index = users.findIndex((user) => user.id === editUserId);
 
+    newUsers[index] = editedContact;
+
+    setUser(newUsers);
+    setEditUserId(null);
+  };
+
+  const handleEditClick = (event, user) => {
+    event.preventDefault();
+    setEditUserId(user.id);
+
+    const formValues = {
+      name: user.name,
+      username: user.username,
+      password: user.password,
+    };
+
+    setEditFormData(formValues);
+  };
+
+  const handleDeleteClick = (userId) => {
+    const newUsers = [...users];
+
+    const index = users.findIndex((user) => user.id === userId);
+
+    newUsers.splice(index, 1);
+
+    setUser(newUsers);
+  };
 
   return (
-
     <AppContainer>
       <Nav />
-      <InnerContainer>
-      </InnerContainer>
+      <InnerContainer></InnerContainer>
       <SomeContent>Your Passwords</SomeContent>
-     
-      
-      <form onSubmit={handleEditFormSubmit} style={{ width: "95%", display: "flex"}}>
-        <table class="table table-dark table-striped shadows">
+
+      <form
+        onSubmit={handleEditFormSubmit}
+        style={{ width: "95%", display: "flex" }}
+      >
+        <table className="table table-dark table-striped shadows">
           <thead>
             <tr>
               <th scope="col">Site</th>
               <th scope="col">UserName</th>
               <th scope="col">Password</th>
-              <th scope="col">Actions</th> 
+              <th scope="col">Actions</th>
             </tr>
           </thead>
 
           <tbody>
             {users.map((user, index) => (
-                <Fragment>
-                  {editUserId === user.id ?( <EditableRow editFormData={editFormData} handleEditFormChange={handleEditFormChange}/> ) : ( <ReadOnlyRow user={user} handleEditClick={handleEditClick} handleDeleteClick={handleDeleteClick}/> ) }
-                </Fragment>
-                ))
-            }
-
+              <Fragment>
+                {editUserId === user.id ? (
+                  <EditableRow
+                    editFormData={editFormData}
+                    handleEditFormChange={handleEditFormChange}
+                  />
+                ) : (
+                  <ReadOnlyRow
+                    user={user}
+                    handleEditClick={handleEditClick}
+                    handleDeleteClick={handleDeleteClick}
+                  />
+                )}
+              </Fragment>
+            ))}
           </tbody>
         </table>
-        </form>
-      
-      
+      </form>
+
       <Button onClick={() => setinfo(!info)}>Add info</Button>
-      <Marginer direction="vertical" margin='2em' />
-      <div style={{ transform: info ? "scale(1)" : "scale(0)",transition: "all 250ms ease-in-out" }}>
-        <h2 style={{"color":"white"}}>Add Details</h2>
+      <Marginer direction="vertical" margin="2em" />
+      <div
+        style={{
+          transform: info ? "scale(1)" : "scale(0)",
+          transition: "all 250ms ease-in-out",
+        }}
+      >
+        <h2 style={{ color: "white" }}>Add Details</h2>
         <form onSubmit={handleAddFormSubmit}>
-          <input type="text" name="name" style={{margin:".3em", fontSize: "1.2em"}} required="required" placeholder="Enter Site" onChange={hanleAddFormChange} />
-          <input type="text" name="username" style={{margin:".3em", fontSize: "1.2em"}} required="required" placeholder="Enter UserName" onChange={hanleAddFormChange} />
-          <input type="password" name="password" style={{margin:".3em", fontSize: "1.2em"}} required="required" placeholder="Enter Password" onChange={hanleAddFormChange} />
-          <Button style={{height:"2.3em"}}>Add</Button>
+          <input
+            type="text"
+            name="name"
+            style={{ margin: ".3em", fontSize: "1.2em" }}
+            required="required"
+            placeholder="Enter Site"
+            onChange={hanleAddFormChange}
+          />
+          <input
+            type="text"
+            name="username"
+            style={{ margin: ".3em", fontSize: "1.2em" }}
+            required="required"
+            placeholder="Enter UserName"
+            onChange={hanleAddFormChange}
+          />
+          <input
+            type="password"
+            name="password"
+            style={{ margin: ".3em", fontSize: "1.2em" }}
+            required="required"
+            placeholder="Enter Password"
+            onChange={hanleAddFormChange}
+          />
+          <Button style={{ height: "2.3em" }}>Add</Button>
         </form>
       </div>
-
-
     </AppContainer>
   );
-}
+};
 
 export default HomeVault;
